a1c847be6b2d77a8e495dd68ee4c5c8e8bf3f125
Nadav <mrnad12@gmail.com>
2019-04-03 21:52:47 +0300

    added menu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import 'semantic-ui-css/semantic.min.css';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
+import * as serviceWorker from "./serviceWorker";
+import { render } from "react-dom";
+import { createBrowserHistory as createHistory } from 'history'
+import thunk from 'redux-thunk';
+import {routerReducer, routerMiddleware} from 'react-router-redux';
+import {
+    BrowserRouter as Router,
+    Route,
+  } from "react-router-dom";
+  import { createStore, combineReducers, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
+import {accountReducer} from "./redux/reducers/account.reducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const history = createHistory();
+
+let middleware = [createLogger()];
+
+middleware = [...middleware,thunk,routerMiddleware(history)];
+
+const reducers = {
+  account: accountReducer,
+  router: routerReducer
+};
+
+const store = createStore(
+  combineReducers(reducers),
+  {},applyMiddleware(...middleware)
+);
+
+render(
+    <Router store={store}>
+      <Route path="/" component={App} />
+  </Router>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
